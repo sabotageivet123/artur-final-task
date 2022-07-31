b921b682623126527b5e60b431805350e9057be8
Ivet_sabotage <sabotageivet@gmail.com>
2022-07-31 12:27:35 +0300

    initial commit

---
+++ application/server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "net/http"
+  "time"
+)
+
+func appHandler(w http.ResponseWriter, r *http.Request) {
+
+  fmt.Println(time.Now(), "Hello from my new fresh server")
+  if(r.URL.Path == "/ping") {
+    w.Write([]byte(`{"status":"OK"}`))
+  } 
+
+}
+
+func main() {
+  http.HandleFunc("/", appHandler)
+  http.HandleFunc("/ping", appHandler)
+
+
+  log.Println("Started, serving on port 8080")
+  err := http.ListenAndServe(":8080", nil)
+
+  if err != nil {
+    log.Fatal(err.Error())
+  }
+}
