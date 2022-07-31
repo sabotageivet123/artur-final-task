174a45efd8219f058b86c93a64a516d95e53b16d
Ivet_sabotage <sabotageivet@gmail.com>
2022-07-31 16:29:11 +0300

    FINAL-1

---
+++ infrastructure/terraform/jenkins/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  cloud {
+    organization = "sabotage-ivet"
+    workspaces {
+      name = "jenkins"
+    }
+  }
+
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+    Purpose = var.purpose
+  }
+}
---
+++ infrastructure/terraform/jenkins/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "devops7-jenkins-finaltask"
+}
+
+variable "purpose" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "jenkins"
+}
