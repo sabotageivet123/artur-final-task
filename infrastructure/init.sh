353a4d14a3610b47b3ddbd5a50352202ce88e63d
Ivet_sabotage <sabotageivet@gmail.com>
2022-08-01 15:58:44 +0300

    FINAL-4

--- infrastructure/init.sh
+++ infrastructure/init.sh
@@ -12,8 +12,33 @@ function init-infra-dev() {
   ansible-playbook -i hosts ./kubernetes-master/install-kubernetes.yaml;
   ansible-playbook -i hosts ./kubernetes-master/create-master.yaml;
   ansible-playbook -i hosts ./kubernetes-node/add-workers.yaml;
+  ansible-playbook -i hosts ./kubernetes-master/generate-key.yaml;
   cd $CURDIR
   cd $TERDIR/jenkins; terraform init && terraform apply -auto-approve; cd $CURDIR;
+  cd $ANSDIR;
+  ansible-playbook -i hosts ./jenkins-role/insall-docker.yaml;
+  ansible-playbook -i hosts ./jenkins-role/generate-key.yaml;
+  cd $CURDIR
+}
+
+function gh_add_key() {
+    echo 'Add key to github'
+    KEY='{"title":"Kuber","key":"'$(cat /tmp/key_ed25519_kuber)'","read_only":true}'
+    TOKEN=$(cat /tmp/token)
+    curl \
+      -X POST \
+      -H "Accept: application/vnd.github+json" \
+      -H "Authorization: token $TOKEN" \
+      https://api.github.com/repos/sabotageivet123/artur-final-task/keys \
+      -d "$KEY"
+    echo 'Add key Jenkins to github'
+    KEY='{"title":"Jenkins","key":"'$(cat /tmp/key_ed25519_jenkins)'","read_only":true}'
+    curl \
+      -X POST \
+      -H "Accept: application/vnd.github+json" \
+      -H "Authorization: token $TOKEN" \
+      https://api.github.com/repos/sabotageivet123/artur-final-task/keys \
+      -d "$KEY"
 }
 
 "$@"
